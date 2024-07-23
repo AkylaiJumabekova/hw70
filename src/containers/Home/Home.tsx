c14e4a399f67bee09cd25f9be6fc653e33544872
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-07-23 18:23:01 +0600

    added modal to ny code

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectContacts } from '../../store/contactsSlice';
 import { Contact } from '../../types';
 import { useNavigate } from 'react-router-dom';
+import ContactModal from '../../components/ContactModal/ContactModal';
 
 const Home: React.FC = () => {
     const contacts = useAppSelector(selectContacts);
     const navigate = useNavigate();
 
+    const [selectedContact, setSelectedContact] = useState<Contact | null>(null);
+
     const handleAddContact = () => {
         navigate('/new-contact');
+    };
+
+    const handleContactClick = (contact: Contact) => {
+        setSelectedContact(contact);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedContact(null);
     };
 
     return (
@@ -20,7 +31,11 @@ const Home: React.FC = () => {
             </button>
             <div>
                 {contacts.map((contact: Contact) => (
-                    <div key={contact.id} className="card mb-2">
+                    <div
+                        key={contact.id}
+                        className="card mb-2"
+                        onClick={() => handleContactClick(contact)}
+                    >
                         <div className="card-body">
                             <h5 className="card-title">{contact.name}</h5>
                             <p className="card-text">{contact.phone}</p>
@@ -30,6 +45,25 @@ const Home: React.FC = () => {
                     </div>
                 ))}
             </div>
+
+            {selectedContact && (
+                <ContactModal
+                    show={!!selectedContact}
+                    title={selectedContact.name}
+                    onClose={handleCloseModal}
+                >
+                    <div className="modal-body">
+                        <p><strong>Phone:</strong> {selectedContact.phone}</p>
+                        <p><strong>Email:</strong> {selectedContact.email}</p>
+                        <img src={selectedContact.photo} alt={selectedContact.name} style={{ width: '100px', height: '100px' }} />
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary" onClick={handleCloseModal}>
+                            Close
+                        </button>
+                    </div>
+                </ContactModal>
+            )}
         </div>
     );
 };
